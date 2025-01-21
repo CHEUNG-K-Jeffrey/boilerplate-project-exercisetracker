59a6bf892bb99fd8064ec37e5956caa42e636d32
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2025-01-21 14:52:24 -0800

    Add GET and POST method stub for /api/users

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.get('/api/users', (req, res) => {
+  res.status(501);
+})
 
-
-
+app.post('/api/users', (req, res) => {
+  res.status(501);
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
