4595e3eaa813faf24b3b0359a8552129db2474fb
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2025-01-22 17:42:59 -0800

    Add GET method stub for /api/users/:id/logs

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ app.post('/api/users/:id/exercises', (req, res) => {
   res.status(501);
 })
 
+app.get('/api/users/:id/logs', (req, res) => {
+  res.status(501);
+})
+
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
